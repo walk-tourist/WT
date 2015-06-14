9d6007059e054084340a93d86de869f372335f69
USER <ability-to-develop2727@ezweb.ne.jp>
2015-06-14 15:51:47 +0900

    BGM再生処理の追加 MDFに効果音再生の為のSoundPoolを追加

---
+++ WT/Walk-Tourist/app/src/main/java/wt/walk_tourist/sound/BGM_Player_Service.java
@@ -0,0 +1,97 @@
+package wt.walk_tourist.sound;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.IOException;
+
+/**
+ * Created by taguchi on 2015/06/09.
+ * BGM再生
+ */
+public class BGM_Player_Service extends Service {
+
+    final IBinder binder = new MyBinder();
+
+    private MediaPlayer mMp;
+    private int mPlayBGMResId = 0;
+
+    public class MyBinder extends Binder {
+        public BGM_Player_Service getService(){
+            return BGM_Player_Service.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId )
+    {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        releaseMp();
+        super.onDestroy();
+    }
+
+    private void releaseMp()
+    {
+        if( null != mMp )
+        {
+            mMp.release();
+            mMp = null;
+        }
+    }
+
+    private void changeBGM(int bgmResId)
+    {
+        releaseMp();
+        mMp = MediaPlayer.create(this,bgmResId);
+        mMp.setLooping(true);
+        mPlayBGMResId = bgmResId;
+    }
+
+    public void startBGM(int bgmResId)
+    {
+        if( mPlayBGMResId != bgmResId )
+        {
+            changeBGM(bgmResId);
+        }
+        mMp.start();
+    }
+
+    public void stopBGM()
+    {
+        if( null != mMp )
+        {
+            mMp.stop();
+            try {
+                mMp.prepare();
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Log.e("stopBGM", "IllegalStateException");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                Log.e("stopBGM", "IOException");
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- WT/Walk-Tourist/app/src/main/java/wt/walk_tourist/wt_fragment/WT_MainDisplayFragment.java
+++ WT/Walk-Tourist/app/src/main/java/wt/walk_tourist/wt_fragment/WT_MainDisplayFragment.java
@@ -1,6 +1,11 @@
 package wt.walk_tourist.wt_fragment;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +19,21 @@ import wt.walk_tourist.R;
  */
 public abstract class WT_MainDisplayFragment extends WT_Fragment {
 
-    public WT_MainDisplayFragment() {}
+    protected SoundPool mSoundPool;
+
+    public WT_MainDisplayFragment() {
+
+        if ( Build.VERSION.SDK_INT >= 21 ) {
+            // SoundPoolはAPI21以降、インスタンスをBuilderから取得する。
+            SoundPool.Builder mSoundPoolBuilder = new SoundPool.Builder();
+            mSoundPoolBuilder.setMaxStreams(5);
+            mSoundPoolBuilder.setAudioAttributes(new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_GAME).build());
+            mSoundPool = mSoundPoolBuilder.build();
+        }else{
+            // 過去API用
+            mSoundPool = new SoundPool(5, AudioManager.STREAM_MUSIC,0);
+        }
+    }
 
     public enum MDF_NAME{
         MDF_BASE,
@@ -40,6 +59,29 @@ public abstract class WT_MainDisplayFragment extends WT_Fragment {
         mListener = (MainFragmentListener)activity;
     }
 
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+
+        mSoundPool.release();
+    }
+
+    // 画面の向きを取得する
+    public int getScreenOrientation()
+    {
+        return ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+    }
+
+    // 画面での再生曲を取得する
+    //public abstract int getBGMResId();
+    public int getBGMResId()
+    {
+        return R.raw.bgm01;
+    }
+
+
+    // 本フラグメント(MDF)破棄時に破棄する必要がある子フラグメント(PDF)を破棄する
     public abstract void removeChildFragment();
 
 }
